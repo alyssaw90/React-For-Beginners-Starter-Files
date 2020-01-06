a8bc0c00d638977d88645da44379a663dc8a36df
Alyssa Williams <alyssaw90@gmail.com>
2020-01-05 20:55:26 -0800

    completed 1-17

--- catch-of-the-day/src/components/Inventory.js
+++ catch-of-the-day/src/components/Inventory.js
@@ -1,10 +1,13 @@
 import React from 'react';
+import AddFishForm from './AddFishForm';
 
 class Inventory extends React.Component {
     render() {
         return (
             <div className="Inventory">
-                <h1>Inventory</h1>
+                <h2>Inventory</h2>
+                <AddFishForm addFish={this.props.addFish} />
+                <button onClick={this.props.loadSampleFishes}>Load Sample Fishes</button>
             </div>
         )
     }
